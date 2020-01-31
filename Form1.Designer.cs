af442ff92f47f1e259c799a8b765784bc69c4b01
Mughis Mirza <58638379+noobmaster95@users.noreply.github.com>
2020-01-31 23:49:43 +0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace NFTimer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.days = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.hrs = new System.Windows.Forms.Label();
+            this.mins = new System.Windows.Forms.Label();
+            this.secs = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.panel2);
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.Name = "panel1";
+            // 
+            // panel3
+            // 
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Controls.Add(this.label2);
+            resources.ApplyResources(this.panel3, "panel3");
+            this.panel3.Name = "panel3";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.ForeColor = System.Drawing.Color.PeachPuff;
+            this.label2.Name = "label2";
+            // 
+            // label9
+            // 
+            resources.ApplyResources(this.label9, "label9");
+            this.label9.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label9.ForeColor = System.Drawing.Color.DarkOrange;
+            this.label9.Name = "label9";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.ForeColor = System.Drawing.Color.LightCoral;
+            this.label1.Name = "label1";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.Desktop;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.label11);
+            this.panel2.Controls.Add(this.label10);
+            this.panel2.Controls.Add(this.label8);
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.days);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.hrs);
+            this.panel2.Controls.Add(this.mins);
+            this.panel2.Controls.Add(this.secs);
+            this.panel2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            resources.ApplyResources(this.panel2, "panel2");
+            this.panel2.Name = "panel2";
+            // 
+            // label11
+            // 
+            resources.ApplyResources(this.label11, "label11");
+            this.label11.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label11.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label11.Name = "label11";
+            // 
+            // label10
+            // 
+            resources.ApplyResources(this.label10, "label10");
+            this.label10.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label10.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label10.Name = "label10";
+            // 
+            // label8
+            // 
+            resources.ApplyResources(this.label8, "label8");
+            this.label8.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label8.ForeColor = System.Drawing.Color.LightCoral;
+            this.label8.Name = "label8";
+            // 
+            // label7
+            // 
+            resources.ApplyResources(this.label7, "label7");
+            this.label7.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label7.ForeColor = System.Drawing.Color.LightCoral;
+            this.label7.Name = "label7";
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label6.ForeColor = System.Drawing.Color.LightCoral;
+            this.label6.Name = "label6";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.ForeColor = System.Drawing.Color.LightCoral;
+            this.label4.Name = "label4";
+            // 
+            // days
+            // 
+            resources.ApplyResources(this.days, "days");
+            this.days.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.days.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.days.Name = "days";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label5.Name = "label5";
+            // 
+            // hrs
+            // 
+            resources.ApplyResources(this.hrs, "hrs");
+            this.hrs.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.hrs.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.hrs.Name = "hrs";
+            // 
+            // mins
+            // 
+            resources.ApplyResources(this.mins, "mins");
+            this.mins.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.mins.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.mins.Name = "mins";
+            // 
+            // secs
+            // 
+            resources.ApplyResources(this.secs, "secs");
+            this.secs.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.secs.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.secs.Name = "secs";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label days;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label hrs;
+        private System.Windows.Forms.Label mins;
+        private System.Windows.Forms.Label secs;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NFTimer
+{
+    public partial class Form1 : Form
+    {
+        DateTime startDate;
+        int counter;
+        int top;
+
+
+        public Form1()
+        {
+            startDate = DateTime.Now;
+            top = 0;
+            counter = 0;
+            InitializeComponent();
+            StartTimer();
+        }
+
+        void StartTimer()
+        {
+            Timer timer = new Timer();
+            timer.Interval = 1000;
+            timer.Enabled = true;
+            timer.Tick += Timer_Tick;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            var d = DateTime.Now.Subtract(startDate);
+            Set(d);
+            
+            if(counter > 300)
+                counter = 0;
+            
+            if (counter == 0)
+                SetLine();
+            
+            counter++;
+        }
+
+        void SetLine()
+        {
+            string path = Directory.GetCurrentDirectory() + @"\lines.txt";
+            var list = File.ReadAllLines(path);
+            if(top < list.Length)
+            {
+                label2.Text =   list[top];
+                top++;
+            }
+            else
+            {
+                top = 0;
+            }
+        }
+
+        void Set(TimeSpan span)
+        {
+            days.Text = span.Days.ToString();
+            hrs.Text = span.Hours.ToString();
+            mins.Text = span.Minutes.ToString();
+            secs.Text = span.Seconds.ToString();
+        }
+
+    }
+}
